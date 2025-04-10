839204c1c3dcedd5b658e95e8fc3a117a596f691
yeljayad <yeljayad@cloud-invest.com>
2025-04-10 14:43:09 +0100

    Fix library name casing in Package.swift for consistency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "EquitiMasterLib",
+            name: "equitimasterlib",
             targets: ["EquitiMasterLibWrapper"]),
     ],
     dependencies: [
@@ -33,4 +33,3 @@ let package = Package(
         )
     ]
 )
-
