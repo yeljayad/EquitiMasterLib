af46d85220e95d109cd5fa20dc0bb90180766651
yeljayad <yeljayad@cloud-invest.com>
2025-04-10 14:54:31 +0100

    Update Swift tools version and refactor package structure for consistency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.7
+// This Swift package requires Swift 5.7 or later.
 
 import PackageDescription
 
@@ -9,26 +9,33 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Defines the public library product.
         .library(
             name: "equitimasterlib",
-            targets: ["EquitiMasterLibWrapper"]),
+            targets: ["EquitiMasterLibWrapper"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/leantechnologies/link-sdk-ios-distribution", .upToNextMajor(from: "3.0.0"))
+        // Lean Technologies Link SDK
+        .package(
+            url: "https://github.com/leantechnologies/link-sdk-ios-distribution",
+            .upToNextMajor(from: "3.0.0")
+        )
     ],
     targets: [
+        // Binary target for the prebuilt XCFramework
         .binaryTarget(
-            name: "EquitiMasterLib", 
+            name: "equitimasterlib",
             url: "https://firebasestorage.googleapis.com/v0/b/cips-dev-b82d8.appspot.com/o/payment-sdk%2Fios%2Fmaster%2Fmaster.xcframework.zip?alt=media&token=081c7eb5-5e16-4006-a396-0d0e2e811af1",
             checksum: "db48022350da1f80fd1e33dc91d2795818e73b153ce57df3f380476b9fba8011"
         ),
+        // Wrapper target that exposes the functionality
         .target(
             name: "EquitiMasterLibWrapper",
             dependencies: [
-                "EquitiMasterLib",
+                "equitimasterlib",
                 .product(name: "LinkSDK", package: "link-sdk-ios-distribution")
-            ],
+            ]
         )
     ]
 )
