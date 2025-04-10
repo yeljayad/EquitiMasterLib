755ada00feca9f11d3077f8cf50b097b9cce328b
yeljayad <yeljayad@cloud-invest.com>
2025-04-10 11:30:15 +0100

    V1 - MASTER LIB

--- EquitiMasterLib/Package.swift
+++ EquitiMasterLib/Package.swift
@@ -11,14 +11,13 @@ let package = Package(
             name: "EquitiMasterLib",
             targets: ["EquitiMasterLib"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/leantechnologies/link-sdk-ios-distribution", .upToNextMajor(from: "3.0.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "EquitiMasterLib"),
-        .testTarget(
-            name: "EquitiMasterLibTests",
-            dependencies: ["EquitiMasterLib"]
-        ),
+        .binaryTarget(name: "EquitiMasterLib", url: "https://firebasestorage.googleapis.com/v0/b/cips-dev-b82d8.appspot.com/o/payment-sdk%2Fios%2Fmaster%2Fmaster.xcframework.zip?alt=media&token=081c7eb5-5e16-4006-a396-0d0e2e811af1", checksum: "db48022350da1f80fd1e33dc91d2795818e73b153ce57df3f380476b9fba8011"),
     ]
 )
+
