abb1f6170bf9c0a01e4bc1eb6199bcd813601fc6
yeljayad <yeljayad@cloud-invest.com>
2025-04-10 12:22:05 +0100

    Refactor package structure and naming conventions for EquitiMasterLib

--- Package.swift
+++ Package.swift
@@ -17,7 +17,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "equitimasterlib", url: "https://firebasestorage.googleapis.com/v0/b/cips-dev-b82d8.appspot.com/o/payment-sdk%2Fios%2Fmaster%2Fmaster.xcframework.zip?alt=media&token=081c7eb5-5e16-4006-a396-0d0e2e811af1", checksum: "db48022350da1f80fd1e33dc91d2795818e73b153ce57df3f380476b9fba8011"),
+        .binaryTarget(name: "equitimasterlib", 
+        url: "https://firebasestorage.googleapis.com/v0/b/cips-dev-b82d8.appspot.com/o/payment-sdk%2Fios%2Fmaster%2Fmaster.xcframework.zip?alt=media&token=081c7eb5-5e16-4006-a396-0d0e2e811af1",
+        checksum: "db48022350da1f80fd1e33dc91d2795818e73b153ce57df3f380476b9fba8011",
+        dependencies: [
+                .product(name: "LeanSDK", package: "link-sdk-ios-distribution")
+            ]
+        )
+        
     ]
 )
 
