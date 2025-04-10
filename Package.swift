17e9bde211bbc364d33ec65b8b8e9d2774bb3eae
yeljayad <yeljayad@cloud-invest.com>
2025-04-10 15:22:12 +0100

    Update Swift tools version to 5.3 and refactor library targets in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EquitiMasterLib",
-            targets: ["EquitiMasterLib", "LeanSDKWrapper"]),
+            targets: ["EquitiMasterLib"]),
+        
+        .library(
+            name: "LeanSDK",
+            targets: ["LeanSDKWrapper"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -30,10 +34,5 @@ let package = Package(
             url: "https://cdn.leantech.me/link/sdk/ios/build.15/LeanSDK.xcframework-build.15.zip", 
             checksum: "e1c34b01bf5d5a9210654ea849f15607c451e585b78ce3fc21fbe8712f3f3d92"
         ),
-        .target(
-            name: "LeanSDKWrapper",
-            dependencies: ["LeanSDK"],
-            path: "Sources/LeanSDKWrapper"
-        )
     ]
 )
