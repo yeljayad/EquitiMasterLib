9ca510777182d505e7c7ece3e646e23f6dd3d4c0
yeljayad <yeljayad@cloud-invest.com>
2025-04-10 12:38:22 +0100

    Add initial implementation of EquitiMasterLib struct

--- Sources/EquitiMasterLib/EquitiMasterLib.swift
+++ Sources/EquitiMasterLib/EquitiMasterLib.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public struct EquitiMasterLib {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
